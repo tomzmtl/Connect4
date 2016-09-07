0c6eb369833bd7b00e15654d8f7883936c3efbd6
Thomas Andreo <tandreo@LMM108.local>
2016-09-07 13:29:49 -0400

    Added coords => index conversion

--- src/js/helpers/grid.js
+++ src/js/helpers/grid.js
@@ -2,3 +2,6 @@ export default null;
 
 
 export const indexToCoords = (width, height, index) => [index % width, Math.floor(index / width)];
+
+
+export const coordsToIndex = (width, height, x, y) => y * width + x;
--- src/js/test/helpers/grid.spec.js
+++ src/js/test/helpers/grid.spec.js
@@ -1,4 +1,4 @@
-import { indexToCoords } from '../../helpers/grid';
+import { coordsToIndex, indexToCoords } from '../../helpers/grid';
 
 describe('Grid Helpers', () => {
   it('Calculates coords from index', () => {
@@ -9,5 +9,15 @@ describe('Grid Helpers', () => {
     expect(indexToCoords(7, 6, 8)).toEqual([1, 1]);
     expect(indexToCoords(7, 6, 18)).toEqual([4, 2]);
     expect(indexToCoords(4, 4, 15)).toEqual([3, 3]);
+  });
+
+  it('Calculates index from coords', () => {
+    expect(coordsToIndex(3, 3, 0, 0)).toEqual(0);
+    expect(coordsToIndex(4, 3, 3, 0)).toEqual(3);
+    expect(coordsToIndex(3, 3, 0, 2)).toEqual(6);
+    expect(coordsToIndex(7, 6, 0, 1)).toEqual(7);
+    expect(coordsToIndex(7, 6, 1, 1)).toEqual(8);
+    expect(coordsToIndex(7, 6, 4, 2)).toEqual(18);
+    expect(coordsToIndex(4, 4, 3, 3)).toEqual(15);
   });
 });
