842afea27abbca9fb3f56c2d9b7c354f309c7d2e
Thomas Andreo <thomas.andreo@gmail.com>
2017-02-16 22:00:17 -0500

    Updated port config

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const app = express();
 const path = require('path');
 
 
+const PORT = process.env.PORT || 3000;
+
+
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Example app listening on port 3000!')
 });
