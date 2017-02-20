e211aedba0416dca9d3a36f8c31ed2e27ee60f03
Thomas Andreo <thomas.andreo@gmail.com>
2017-02-20 18:05:03 -0500

    New main menu UI

--- src/ui/components/MainMenu.jsx
+++ src/ui/components/MainMenu.jsx
@@ -10,13 +10,11 @@ const Component = ({ ui, onClose, onReset }) => {
 
   return (
     <div className={classes}>
-      <button className="button new" onClick={onReset}>New game</button>
-      <button className="button close" onClick={onClose}>
-        <svg className="svg fill-primary" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-          <path d="M0 0h24v24H0z" fill="none" />
-        </svg>
-      </button>
+      <div className="menu">
+        <button className="button new" onClick={onReset}>New game</button>
+        {/* <button className="button players">Edit players</button>*/}
+        <button className="button close" onClick={onClose}>Close menu</button>
+      </div>
     </div>
   );
 };
